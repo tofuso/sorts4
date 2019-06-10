b35d80763772ac0caedc0ab605717747cf5bc282
tofuso <tofu.so.shioaji@gmail.com>
2019-06-10 22:16:10 +0900

    InsertionSortを追加

--- sorts/sorts.go
+++ sorts/sorts.go
@@ -1,21 +1,34 @@
 package sorts
 
 // InsertionSort 挿入ソート
-func InsertionSort(a *[]int) {
+func InsertionSort(a []int) {
+	var (
+		i, j, key int
+	)
+
+	for j = 1; j < len(a); j++ {
+		key = a[j]
+		i = j - 1
+		for i >= 0 && a[i] > key {
+			a[i+1] = a[i]
+			i--
+		}
+		a[i+1] = key
+	}
 	return
 }
 
 // MergeSort マージソート
-func MergeSort(a *[]int) {
+func MergeSort(a []int) {
 	return
 }
 
 // HeapSort ヒープソート
-func HeapSort(a *[]int) {
+func HeapSort(a []int) {
 	return
 }
 
 // QuickSort クイックソート
-func QuickSort(a *[]int) {
+func QuickSort(a []int) {
 	return
 }

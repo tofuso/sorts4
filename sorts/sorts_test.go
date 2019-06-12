1b1f8dbecd88d43bb7551109f307454dfbac8c61
tofuso <tofu.so.shioaji@gmail.com>
2019-06-12 15:55:30 +0900

    テストでソートされたまま、実行されてしまう問題を修正。クイックソートのテストを追加。

--- sorts/sorts_test.go
+++ sorts/sorts_test.go
@@ -5,11 +5,13 @@ import (
 )
 
 var (
-	numbers = []int{89, 94, 34, 78, 6, 19, 4, 95, 62, 79}
+	source = []int{89, 94, 34, 78, 6, 19, 4, 95, 62, 79}
 	correct = []int{4, 6, 19, 34, 62, 78, 79, 89, 94, 95}
 )
 
 func TestInsertionSort(t *testing.T) {
+	numbers := make([]int, len(source))
+	copy(numbers, source)
 	InsertionSort(numbers)
 	for i, num := range numbers {
 		if num != correct[i] {
@@ -19,6 +21,8 @@ func TestInsertionSort(t *testing.T) {
 }
 
 func TestMergeSort(t *testing.T) {
+	numbers := make([]int, len(source))
+	copy(numbers, source)
 	MergeSort(numbers)
 	for i, num := range numbers {
 		if num != correct[i] {
@@ -28,7 +32,20 @@ func TestMergeSort(t *testing.T) {
 }
 
 func TestHeapSort(t *testing.T){
+	numbers := make([]int, len(source))
+	copy(numbers, source)
 	HeapSort(numbers)
+	for i, num := range numbers {
+		if num != correct[i] {
+			t.Fatal("not correct: ", numbers, correct)
+		}
+	}
+}
+
+func TestQuickSort(t *testing.T){
+	numbers := make([]int, len(source))
+	copy(numbers, source)
+	QuickSort(numbers)
 	for i, num := range numbers {
 		if num != correct[i] {
 			t.Fatal("not correct: ", numbers, correct)
